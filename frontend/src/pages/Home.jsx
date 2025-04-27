90b4449ea0ca6ff09815633d480217f28566eaad
dhruvpatel-05 <dhruvp2106@gmail.com>
2025-04-26 23:17:17 -0700

    home page text changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,9 +12,10 @@ function Home() {
           <h1>Welcome to AI Course Insights!</h1>
         </section>
         <div className="instructions">
-          <p>1. To start, add your classes by selecting the create button.</p>
-          <p>2. Then upload homework, quizzes, and tests by selecting the upload button.</p>
-          <p>3. After this, we will provide valuable insights about your performance and suggested improvements!</p>
+          <p>1. To start, sign up and log in to our website!</p>
+          <p>2. Next, add your classes by selecting the create tab.</p>
+          <p>3. Then add homework, quizzes, and tests by selecting the upload tab.</p>
+          <p>4. After this, we will provide valuable insights about your performance and suggest improvements!</p>
         </div>
       </div>
     </div>
